f2f25e11127da0052ff80d21fcf737c9c86cbda9
”onizawahayato” <onizawalala@gmail.com>
2020-06-22 11:05:58 +0000

    net更新２

---
+++ app/Http/Controllers/Admin/Net1Controller.php
@@ -0,0 +1,81 @@
+<?php
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Diagnosis;
+use Storage;
+
+class Net1Controller extends Controller
+{
+    public function add()
+    {
+        return view('admin.net1.create');
+    }
+    public function create(Request $request)
+    {
+        $this->validate($request, Diagnosis::$rules);
+        $net1 = new Diagnosis;
+        $form = $request->all();
+
+   
+
+        unset($form['_token']);
+     
+        $net1->fill($form);
+        $net1->save();
+        $symptom_id = $form['symptom_id'];
+
+        return redirect(route('symptom_detail', ['symptom_id' => $symptom_id]));
+    }
+
+    public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            $posts = Diagnosis::where('title', $cond_title)->get();
+        } else {
+            $posts = Diagnosis::all();
+        }
+        return view('admin.net1.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
+  
+    public function edit(Request $request)
+    {
+        $net1 = Diagnosis::find($request->id);
+        if (empty($net1)) {
+            abort(404);
+        }
+        return view('admin.net1.edit', ['net1_form' => $net1]);
+    }
+
+
+    public function update(Request $request)
+    {
+        $this->validate($request, Diagnosis::$rules);
+        $net1 = Diagnosis::find($request->id);
+        $net1_form = $request->all();
+        if ($request->remove == 'true') {
+            $net1_form['image_path'] = null;
+        } elseif ($request->file('image')) {
+            $path = Storage::disk('s3')->putFile('/', $form['image'], 'public');
+            $net1->image_path = Storage::disk('s3')->url($path);
+        } else {
+            $net1_form['image_path'] = $net1->image_path;
+        }
+
+        unset($net1_form['_token']);
+        unset($net1_form['image']);
+        unset($net1_form['remove']);
+        $net1->fill($net1_form)->save();
+
+
+        return redirect('admin/net1/');
+    }
+    public function delete(Request $request)
+    {
+        $net1 = Diagnosis::find($request->id);
+        $net1->delete();
+        return redirect('admin/net1/');
+    }
+}
--- app/Http/Controllers/Admin/NetController.php
+++ app/Http/Controllers/Admin/NetController.php
@@ -6,55 +6,56 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Symptom;
 use Storage;
+
 class NetController extends Controller
 {
     public function add()
-  {
-      return view('admin.net.create');
-  }
-  public function create(Request $request)
-  {
-     $this->validate($request, Symptom::$rules);
-     $net = new Symptom;
-     $form = $request->all();
+    {
+        return view('admin.net.create');
+    }
+    public function create(Request $request)
+    {
+        $this->validate($request, Symptom::$rules);
+        $net = new Symptom;
+        $form = $request->all();
 
-     if (isset($form['image'])) {
-        $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
-        $net->image_path = Storage::disk('s3')->url($path);
-     } else {
-          $net->image_path = null;
-     }
+        if (isset($form['image'])) {
+            $path = Storage::disk('s3')->putFile('/', $form['image'], 'public');
+            $net->image_path = Storage::disk('s3')->url($path);
+        } else {
+            $net->image_path = null;
+        }
 
-     unset($form['_token']);
-     unset($form['image']);
-     $net->fill($form);
-     $net->save();
+        unset($form['_token']);
+        unset($form['image']);
+        $net->fill($form);
+        $net->save();
 
-     return redirect('admin/net/create');
-  }
+        return redirect('admin/net/create');
+    }
 
-  public function index(Request $request)
-  {
-     $cond_title = $request->cond_title;
-     if ($cond_title != '') {
-          $posts = Symptom::where('title', $cond_title)->get();
-     } else {
-          $posts = Symptom::all();
-      }
-      return view('admin.net.index', ['posts' => $posts, 'cond_title' => $cond_title]);
-  }  
+    public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            $posts = Symptom::where('title', $cond_title)->get();
+        } else {
+            $posts = Symptom::all();
+        }
+        return view('admin.net.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
   
-  public function edit(Request $request)
-  {
-      $net = Symptom::find($request->id);
-      if (empty($net)) {
-        abort(404);    
-      }
-      return view('admin.net.edit', ['net_form' => $net]);
-  }
+    public function edit(Request $request)
+    {
+        $net = Symptom::find($request->id);
+        if (empty($net)) {
+            abort(404);
+        }
+        return view('admin.net.edit', ['net_form' => $net]);
+    }
 
 
-   public function update(Request $request)
+    public function update(Request $request)
     {
         $this->validate($request, Symptom::$rules);
         $net = Symptom::find($request->id);
@@ -62,7 +63,7 @@ class NetController extends Controller
         if ($request->remove == 'true') {
             $net_form['image_path'] = null;
         } elseif ($request->file('image')) {
-            $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+            $path = Storage::disk('s3')->putFile('/', $form['image'], 'public');
             $net->image_path = Storage::disk('s3')->url($path);
         } else {
             $net_form['image_path'] = $net->image_path;
@@ -76,10 +77,10 @@ class NetController extends Controller
 
         return redirect('admin/net/');
     }
-  public function delete(Request $request)
-  {
-      $net = Condition::find($request->id);
-      $net->delete();
-      return redirect('admin/net/');
-  }  
+    public function delete(Request $request)
+    {
+        $net = Condition::find($request->id);
+        $net->delete();
+        return redirect('admin/net/');
+    }
 }
