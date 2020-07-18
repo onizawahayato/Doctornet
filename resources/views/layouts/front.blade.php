bb848aa60ad0e1a1e0ca4b8952f693034600cfe8
”onizawahayato” <onizawalala@gmail.com>
2020-07-18 08:00:19 +0000

    MENTA前

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -28,6 +28,7 @@
                     <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
                     </a>
+                    
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -65,6 +66,11 @@
                 </div>
             </nav>
 
+			
+		
+		
+	</div>
+</nav>
             <main class="py-4">
                 @yield('content')
             </main>
